c7bc3b233b45866690947c0290f26550ec12c74c
Nguyen Chuong <nguyennguyenchuongst@gmail.com>
2024-09-13 23:23:38 +0700

    add file

---
+++ src/Buoi04BaiTap10.java
@@ -0,0 +1,9 @@
+public class Buoi04BaiTap10 {
+    public static void main(String[] args) {
+        //alsdkfal
+        //asdjfl
+        //asldkfjl
+        //lsdfjla
+        System.out.println("FIX");
+    }
+}
