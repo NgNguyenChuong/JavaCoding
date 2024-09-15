f266f69714a69a19bcc370ac749228c8c7cbf08d
Nguyen Chuong <nguyennguyenchuongst@gmail.com>
2024-09-15 15:19:11 +0700

    add file

--- src/Buoi04BaiTap10.java
+++ src/Buoi04BaiTap10.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Buoi04BaiTap10 {
     public static void main(String[] args) {
-        //alsdkfal
-        //asdjfl
-        //asldkfjl
-        //lsdfjla
-        System.out.println("FIX");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int number = scanner.nextInt();
+
+        int countOdd = 0;
+        int countEven = 0;
+
+        while(number > 0) {
+            int temp = number % 10;
+            if(temp % 2 == 0) countEven++;
+            else countOdd++;
+            number /= 10;
+        }
+        System.out.println(countEven + " " + countOdd);
+
     }
 }
