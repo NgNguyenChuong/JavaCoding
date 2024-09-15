a7db0e477847fdd2d51d30b1d22f014cd0024091
Nguyen Chuong <nguyennguyenchuongst@gmail.com>
2024-09-15 14:29:40 +0700

    add file

--- src/Buoi04BaiTap06.java
+++ src/Buoi04BaiTap06.java
@@ -10,12 +10,13 @@ public class Buoi04BaiTap06 {
         System.out.println("phan tich thua so nguyen to cua n!");
         System.out.print("Enter N: ");
         int n = scanner.nextInt();
-        for(int i = 2; i <= n/2 + 1; i++) {
-            while(n > 0 && n % i == 0) {
+        for(int i = 2; i <= Math.sqrt(n) ; i++) {
+            while(n % i == 0) {
                 System.out.print(i + " ");
                 n /= i;
             }
         }
+        if(n>1) System.out.println(n);
 
 
 
