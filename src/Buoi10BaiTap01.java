6e1d0c13248ddc7d89cc051905921040463498ea
Nguyen Chuong <nguyennguyenchuongst@gmail.com>
2024-09-19 14:29:38 +0700

    add file

--- src/Buoi10BaiTap01.java
+++ src/Buoi10BaiTap01.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
+
 public class Buoi10BaiTap01 {
+    public static int solve(int n) {
+        if(n == 1) return 1;
+        return n + solve(n - 1);
+    }
     public static void main(String[] args) {
-        //dkfjls
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        System.out.print("enter the number to calculate S = 1 + 2 + ... + n: ");
+        n = scanner.nextInt();
+        System.out.println("Result = " + solve(n));
     }
 }
--- src/Buoi10BaiTap02.java
+++ src/Buoi10BaiTap02.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Buoi10BaiTap02 {
+    public static long solve(int n) {
+        if( n == 1) return 1;
+        return (long)Math.pow(n,2) + solve(n-1);
+    }
     public static void main(String[] args) {
-        //sldfjsdlfwercmv
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(solve(n));
     }
 }
