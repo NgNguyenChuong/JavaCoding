a403ed705a90c3d4859c09ee10481a536157da54
Nguyen Chuong <nguyennguyenchuongst@gmail.com>
2024-09-19 15:03:53 +0700

    add file

--- src/Buoi10BaiTap04.java
+++ src/Buoi10BaiTap04.java
@@ -1,5 +1,14 @@
-public class Buoi10BaiTap04 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Buoi10BaiTap04 {
+    public static int solve(int n) {
+        if(n <= 0) return 0;
+        return 1 + solve(n / 10);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter N to count the number of digit : ");
+        int n = scanner.nextInt();
+        System.out.println("Result = " + solve(n));
     }
 }
--- src/Buoi10BaiTap05.java
+++ src/Buoi10BaiTap05.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Buoi10BaiTap05 {
+    public static long solve(int n) {
+        if(n == 1) return 1;
+        return n * solve(n - 1);
+    }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter N to calculate: ");
+        int n = scanner.nextInt();
+        System.out.println("Result = " + solve(n));
 
     }
 }
