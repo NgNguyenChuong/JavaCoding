2d0c0f7b7ac385921c6c69eeecc12519e5c54f07
Nguyen Chuong <nguyennguyenchuongst@gmail.com>
2024-09-19 14:48:44 +0700

    add file

--- src/Buoi10BaiTap03.java
+++ src/Buoi10BaiTap03.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Buoi10BaiTap03 {
+    public static long fibo(int n) {
+        if(n == 1 || n == 2) return 1;
+        return fibo(n - 1) + fibo(n - 2);
+
+    }
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter sequence number of fibo: ");
+        int n = scanner.nextInt();
+        System.out.println("Result = " + fibo(n));
     }
 }
